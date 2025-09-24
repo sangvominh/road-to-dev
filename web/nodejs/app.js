61031b6bbf6ef874be4b0990f43906d767ca7384
Minh Sang <vosang20102005@gmail.com>
2025-09-24 22:41:50 +0700

    feat: setup Express server to serve HTML views

---
+++ web/nodejs/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express()
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.sendFile('./views/index.html', {root: __dirname})
+});
+app.get("/about-me", (req, res) => {
+  res.sendFile('./views/about.html', {root: __dirname})
+});
