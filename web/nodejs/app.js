e6b177c7379e9d5d422f88429a037ac804dc1d09
Minh Sang <vosang20102005@gmail.com>
2025-09-26 20:52:36 +0700

    Refactor and update blog-related routes to use view rendering

--- web/nodejs/app.js
+++ web/nodejs/app.js
@@ -19,38 +19,15 @@ mongoose
   .then((result) => app.listen(3000))
   .catch((err) => console.log("err"));
 
-// mongoose db and mongo sandbox route
-app.get("/create-blog", (req, res) => {
-  const blog = new Blog({
-    title: "my title blog",
-    snippet: "my snippet",
-    body: "my body blog",
-  });
-
-  blog
-    .save()
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
-app.get("/all-blog", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/single-blog", (req, res) => {
-  Blog.findById("68d68fef710b99abfa9917c4")
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err));
-});
-
+// routes
 app.get("/", (req, res) => {
   res.render("index.ejs", { title: "home" });
+});
+
+app.get("/blog", (req, res) => {
+  Blog.find()
+    .then((result) => res.render("blog.ejs", { title: "All blog", blog: result }))
+    .catch((err) => console.log("err"));
 });
 
 app.get("/about-me", (req, res) => {
