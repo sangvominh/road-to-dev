02afd56ac50e6adf62362ed02ee946bc02e00af2
Minh Sang <vosang20102005@gmail.com>
2025-09-28 20:36:07 +0700

    add post & delete req

--- web/nodejs/app.js
+++ web/nodejs/app.js
@@ -6,18 +6,21 @@ const Blog = require("./models/blog");
 const app = express();
 // ejs
 const ejs = require("ejs");
+const { result } = require("lodash");
 app.set("view engine", "ejs");
 
 // morgan & static file
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 
 // connect to database
 const dbURI = "mongodb+srv://vosang20102005_db_user:1234@cluster0.zpjdb3o.mongodb.net/mydb";
 mongoose
   .connect(dbURI)
   .then((result) => app.listen(3000))
-  .catch((err) => console.log("err"));
+  .catch((err) => console.log("error in connect to database", err));
 
 // routes
 app.get("/", (req, res) => {
@@ -27,13 +30,40 @@ app.get("/", (req, res) => {
 app.get("/blog", (req, res) => {
   Blog.find()
     .then((result) => res.render("blog.ejs", { title: "All blog", blog: result }))
-    .catch((err) => console.log("err"));
+    .catch((err) => console.log("error in get blog", err));
+});
+
+app.post("/blog", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => res.redirect("/blog"))
+    .catch((err) => console.log("error in create blog", err));
+});
+
+app.get("/blog/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => res.render("blog-detail.ejs", { title: "Blog Detail", blog: result }))
+    .catch((err) => console.log("error in get blog detail", err));
+});
+
+app.delete("/blog/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => res.json({ redirect: "/blog" }))
+    .catch((err) => console.log("error in delete blog", err));
+});
+
+app.get("/create-blog", (req, res) => {
+  res.render("create-blog.ejs", { title: "create blog" });
 });
 
 app.get("/about-me", (req, res) => {
   res.render("about.ejs", { title: "about-me" });
 });
 
+// route not found
 app.use((req, res) => {
   res.render("404.ejs", { title: "404" });
 });
